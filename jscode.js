6ce4e44f16cc46fd12053b17bae413458cc5c1fe
kyaw-lwin <kyawlwin388@gmail.com>
2022-09-18 23:42:20 +0100

    main page design change

--- jscode.js
+++ jscode.js
@@ -1,6 +1,6 @@
 //intro text
 var i = 0;
-var txt = "Hello, My name is Kyaw. I'm from Sheffield, UK. I used to work in semi-conductor manufacturing industries as an engineer for 15 years.I started to learn coding in 2022 and I am enjoying it since then. It make me happy when I see the websites that I have built."
+var txt = "Hello, My name is Kyaw. I'm from Sheffield, UK. I used to work in semi-conductor manufacturing industries as an engineer for 15 years.I started to learn coding in 2022 and I am enjoying it since then. If you are interested in me or hire me "
 var speed = 50;
 
 function typeWriter() {
